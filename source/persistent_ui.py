632908f407f586b8da89de002b79afd566f4d96f
Dan Le Man <58541834+DanLeEpicMan@users.noreply.github.com>
2023-07-29 21:10:11 -0700

    Finished API connections for verify system; have a working prototype.

--- source/persistent_ui.py
+++ source/persistent_ui.py
@@ -3,6 +3,7 @@ from discord import ui, ButtonStyle
 from discord.ext.commands import Bot
 from source.tools.ui_helper import generate_embed
 from abc import ABCMeta, abstractmethod
+from source.tools.web_tools import check_member_status
 
 
 class BaseView(ui.View):
@@ -15,6 +16,12 @@ class BaseView(ui.View):
 class BasePersistentUI(metaclass=ABCMeta):
     '''
     Base class that all persistent UI buttons should inherit from.
+    ### Parameters
+      `bot`: The `commands.Bot` instance of the bot.\n
+      `config`: The `json` config file containg relevant server information.
+    ### Properties
+      `message`: The ID of the message that the UI will attach itself to.\n
+      `view`: A `BaseView` object containing all UI objects.
     '''
     def __init__(self, *, bot: Bot, config: dict) -> None:
         self.bot = bot
@@ -32,11 +39,18 @@ class BasePersistentUI(metaclass=ABCMeta):
     @abstractmethod
     def view(self) -> BaseView:
         '''
-        Must be overridden and return a subclass of `ui.View`
+        Must be overridden and return a subclass of `BaseView`
         '''
         pass
 
 class Verify(BasePersistentUI):
+    '''
+    The verify button users can press to receive the member role.
+    '''
+    def __init__(self, *, bot: Bot, config: dict) -> None:
+        super().__init__(bot=bot, config=config)
+        self.member_role = discord.Object(config['member_role_id'])
+
     @property
     def message(self) -> int:
         return self.config['verify_message']
@@ -49,8 +63,15 @@ class Verify(BasePersistentUI):
                 style=ButtonStyle.blurple,
                 custom_id='verify-button'
             )
-            async def verify(self, interaction: discord.Interaction, button: ui.Button):
-                await interaction.response.send_message('Pressed!', ephemeral=True)
+            async def verify(self_view, interaction: discord.Interaction, button: ui.Button):
+                await interaction.response.defer(ephemeral=True, thinking=True)
+                member = interaction.user
+                result = check_member_status(member)
+                if result:
+                    await member.add_roles(self.member_role)
+                    await interaction.followup.send('Success!', ephemeral=True)
+                else:
+                    await interaction.followup.send("It doesn't seem like you're a member of Data Science UCSB. Please contact a board member if this is an error.", ephemeral=True)
 
 
         return VerifyView
--- source/tools/web_tools.py
+++ source/tools/web_tools.py
@@ -1,9 +1,32 @@
 import discord
-import aiohttp
+import gspread
+import json
+from datetime import datetime as dt
 
+
+# everything related to checking member status
+class _CacheClient(gspread.Client):
+    '''
+    Adds additional parameters and methods to `Client` to allow for easy caching.
+    '''
+    def __init__(self, auth, session=None):
+        super().__init__(auth, session)
+        self.last_updated = dt.now()
+        self._users = set(self.open_by_key(SHEETS_CONFIG['sheet_id']).sheet1.col_values(SHEETS_CONFIG['user_column']))
+
+    @property
+    def users(self) -> set[str]:
+        now = dt.now()
+        time_difference = now - self.last_updated
+        if time_difference.seconds >= 3600: # update every hour
+            self._users = set(self.open_by_key(SHEETS_CONFIG['sheet_id']).sheet1.col_values(SHEETS_CONFIG['user_column']))
+            self.last_updated = now
+        return self._users
+
+SHEETS_CONFIG = json.load(open('secrets/sheets_config.json'))
+GC: _CacheClient = gspread.service_account(filename='secrets/sheets_credentials.json', scopes=SHEETS_CONFIG['scopes'], client_factory=_CacheClient)
 
 def check_member_status(member: discord.Member) -> bool:
-    '''
-    Checks whether the given `Member` is a registered member of Data Science UCSB.
-    '''
-    return False
+    query = member.name if member.discriminator == '0' else f'{member.name}#{member.discriminator}' # to allow for legacy users
+    return query in GC.users
+    
