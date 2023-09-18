b99785ba0c016da07706a9edb40fdd7dd507aada
Dan Le Man <58541834+DanLeEpicMan@users.noreply.github.com>
2023-09-18 14:29:21 -0700

    Created prototype model for class role menu.

--- source/persistent_ui.py
+++ source/persistent_ui.py
@@ -1,5 +1,5 @@
 import discord
-from discord import ui, ButtonStyle
+from discord import ui, ButtonStyle, SelectOption
 from discord.ext.commands import Bot
 from source.tools.ui_helper import generate_embed
 from abc import ABCMeta, abstractmethod
@@ -66,7 +66,7 @@ class Verify(BasePersistentUI):
                 style=ButtonStyle.blurple,
                 custom_id='verify-button'
             )
-            async def verify(self_view, interaction: discord.Interaction, button: ui.Button):
+            async def verify(view_self, interaction: discord.Interaction, button: ui.Button):
                 await interaction.response.defer(ephemeral=True, thinking=True)
                 member = interaction.user
                 result = check_member_status(member)
@@ -76,5 +76,35 @@ class Verify(BasePersistentUI):
                 else:
                     await interaction.followup.send("It doesn't seem like you're a member of Data Science UCSB. Please contact a board member if this is an error.", ephemeral=True)
 
-
         return VerifyView
+
+class ClassRoleMenu(BasePersistentUI):
+    def __init__(self, *, bot: Bot, config: dict) -> None:
+        super().__init__(bot=bot, config=config)
+        self.roles = {
+            name: discord.Object(role_id)
+            for name, role_id in self.config['roles_config']['class_roles'].items()
+        }
+
+    @property
+    def message(self) -> int:
+        return self.config['roles_config']['role_message']
+    
+    @property
+    def view(self) -> BaseView:
+        class RoleMenu(BaseView):
+            @ui.select(
+                options=[
+                    SelectOption(label=name)
+                    for name in self.roles
+                ],
+                placeholder='Choose your current class year!',
+                custom_id='role-menu'
+            )
+            async def select_role(view_self, interaction: discord.Interaction, menu: discord.ui.Select):
+                role = self.roles[menu.values[0]]
+                await interaction.user.remove_roles(*self.roles.values())
+                await interaction.user.add_roles(role)
+                await interaction.response.send_message(f'Successfully gave you {menu.values[0]}', ephemeral=True)
+
+        return RoleMenu
